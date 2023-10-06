eadb77e2bea43497a6363b6400cdf8cc3e4cacec
girish-yeluri8 <147142789+girish-yeluri8@users.noreply.github.com>
2023-10-06 15:38:06 +0530

    first commit

---
+++ Ayurvedic.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ayurvedic.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Category Name")] //client side validation
+        public string Name { get; set; }
+        [Range(1, 200, ErrorMessage = "Display Order must be between 1-100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
